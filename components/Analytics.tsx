feb714a9175fd5524e738e3f56292d630f118821
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-07-31 17:24:30 +0530

    changed ID

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -4,7 +4,7 @@ import ClickChutney from '@click-chutney/analytics'
 
 export default function Analytics() {
   useEffect(() => {
-    ClickChutney.init('cc_1753962409259_fmawpj88sbh')
+    ClickChutney.init('cc_1753962840054_9d7rrctqzjb')
     ClickChutney.page()
   }, [])
   return null
